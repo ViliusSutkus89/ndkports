243baa36162cdb6c609f0a620b37f8ce8a44fda7
Dan Albert <danalbert@google.com>
2021-06-10 15:49:06 -0700

    Revive the release task.
    Accidentally deleted this in the refactor. This isn't quite the same
    as before because it only builds the full distribution and non of the
    per-package distributions, but it should fix the build.
    
    Change-Id: Idf04a8dae39f0d3de07d2c5ff5c7a7c627cfa231

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,28 @@
 group = "com.android"
 version = "1.0.0-SNAPSHOT"
 
+plugins {
+    distribution
+}
+
 repositories {
     mavenCentral()
     jcenter()
     google()
+}
+
+distributions {
+    main {
+        contents {
+            from("${rootProject.buildDir}/repository")
+            include("**/*.aar")
+            include("**/*.pom")
+        }
+    }
+}
+
+tasks.register("release") {
+    dependsOn(project.getTasksByName("build", true))
+    dependsOn(project.getTasksByName("publish", true))
+    dependsOn(":distZip")
 }
