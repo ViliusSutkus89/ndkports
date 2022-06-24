e063ed00562a569e2baa5967d6ade9fd0888ad03
James Farrell <jamesfarrell@google.com>
2022-06-24 15:32:02 +0000

    Docker build script improvements.
    Put common commandline flags for gradle in ENTRYPOINT so they live in a
    single place.
    
    Make the default Docker behavior be a release build.
    
    Always clean before doing a release build. Not really necessary for
    Kokoro, but safer if we ever need to release from a local build.
    
    Quality of life improvements for using docker locally (as opposed to
    Kokoro):
    
    Allow custom gradle targets to be passed to build_snapshot.
    
    Run docker commands as the current user, to avoid polluting the source
    tree with files owned by root.
    
    Don't start a persistent gradle daemon inside docker. There's no point.
    
    Use a local directory for gradle home, so we can re-use its cache across
    multiple calls to "docker run".
    
    Change-Id: Iac7ffc99c50267d9f7516bf7ab8d744c0efc0724

--- scripts/build_snapshot.sh
+++ scripts/build_snapshot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 docker build -t ndkports .
-# Default command for the docker image handles the NDK location, --stacktrace,
-# task list, etc.
-docker run --rm -v $(pwd):/src ndkports
+TARGETS="${@:-release}"
+docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/src ndkports "${TARGETS}"
