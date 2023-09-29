918ff1201709044130f8f7918cbd72ff61d8d60e
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2023-09-29 05:37:31 +0300

    Remove -beta-1 and -SNAPSHOT version suffix. MavenCentral don't like it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 buildscript {
-    val snapshotSuffix = if (hasProperty("release")) {
-        // We're still tagging releases as betas until we have more thorough
-        // test automation.
-        "-beta-1"
     } else {
-        "-SNAPSHOT"
     }
 
     extra.apply {
-        set("snapshotSuffix", snapshotSuffix)
     }
 }
 
 group = "com.android"
-version = "1.0.0${extra.get("snapshotSuffix")}"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
