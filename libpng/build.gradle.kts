0df538735fc368c1e18d4167ca6f9407654c8359
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2023-10-31 22:54:15 +0200

    bump libpng version

--- libpng/build.gradle.kts
+++ libpng/build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.jvm.tasks.Jar
 val portVersion = "1.6.40"
 
 group = rootProject.group
-version = "${portVersion}-beta-2"
+version = "${portVersion}-beta-3"
 
 plugins {
     id("maven-publish")
