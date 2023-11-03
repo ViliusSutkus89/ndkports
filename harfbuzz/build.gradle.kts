f2a51c0e1d9c2aa410095a1180d39028bb65d6f0
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2023-11-03 11:33:27 +0200

    Remove comments, notes already in issues

--- harfbuzz/build.gradle.kts
+++ harfbuzz/build.gradle.kts
@@ -18,13 +18,8 @@ dependencies {
     val ndkVersionSuffix = rootProject.extra.get("ndkVersionSuffix")
     implementation("com.viliussutkus89.ndk.thirdparty:cairo${ndkVersionSuffix}-static:1.18.0-beta-1")
     implementation("com.viliussutkus89.ndk.thirdparty:fontconfig${ndkVersionSuffix}-static:2.14.2-beta-1")
-
-    // @TODO: freetype/harfbuzz circular dependencies
     implementation("com.viliussutkus89.ndk.thirdparty:freetype${ndkVersionSuffix}-static:2.13.2-beta-3")
     implementation("com.viliussutkus89.ndk.thirdparty:glib2${ndkVersionSuffix}-static:2.78.1-beta-2")
-
-    // @TODO: fix build break when linking shared lib against static graphite2
-//    implementation("com.viliussutkus89.ndk.thirdparty:graphite2${ndkVersionSuffix}-static:1.3.14-beta-1")
 }
 
 ndkPorts {
